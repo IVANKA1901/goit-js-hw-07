33d2c62cdf7965a87cd9d759d1a57620a6ec96cd
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-04-25 15:58:06 +0200

    homework 7 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+///////Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const galleryList = document.querySelector(".gallery");
+
+const itemsGallery = galleryItems
+  .map(
+    (item) => `
+    <li class="gallery__item">
+    <a
+    class="gallery__link"
+    href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"/>
+      </a>
+    </li>
+`
+  )
+  .join("");
+galleryList.innerHTML = itemsGallery;
+// console.log(itemsGallery);
+// console.log(galleryItems);
+
+// Реалізація делегування на ul.gallery і отримання url великого зображення.
+galleryList.addEventListener("click", onImageClick);
+
+function onImageClick(eve) {
+  eve.preventDefault();
+
+  const clickElem = eve.target;
+  //   console.log(clickElem);
+
+  if (clickElem.nodeName !== "IMG") {
+    return;
+  }
+
+  const originImage = clickElem.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${originImage}" width="800" height=600">
+`);
+
+  instance.show();
+  console.log(originImage);
+}
